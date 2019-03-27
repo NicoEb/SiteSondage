71bc13e09938387bdc39ac9d82e8ddc014036d1b
Nicolas EBENER <coladaitp18-eni@ccicampus.fr>
2019-03-27 11:45:28 +0100

    ajout e de la classe sondage

---
+++ SiteSondage/Models/ClassSondage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteSondage.Models
+{
+    public class ClassSondage
+    {
+        public string Question { get; private set; }
+        public string Choix1 { get; private set; }
+        public string Choix2 { get; private set; }
+        public string Choix3 { get; private set; }
+        public string Choix4 { get; private set; }
+
+        public int IdSondage { get; private set; }
+
+        public bool ChoixMultiple { get; private set; }
+
+        public ClassSondage(string question, string choix1, string choix2, string choix3, string choix4, int idSondage, bool choixMultiple)
+        {
+            Question = question;
+            Choix1 = choix1;
+            Choix2 = choix2;
+            Choix3 = choix3;
+            Choix4 = choix4;
+            IdSondage = idSondage;
+            ChoixMultiple = choixMultiple;
+        }
+    }
+}
