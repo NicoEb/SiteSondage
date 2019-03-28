589df5c53c8c22b875b8a26f8d40577272403df0
Admin <Admin@ADMIN-PC>
2019-03-28 01:13:52 +0100

    ajout connexion a la base de donnee

--- SiteSondage/Controllers/SondageController.cs
+++ SiteSondage/Controllers/SondageController.cs
@@ -1,21 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SiteSondage.Models;
 
 namespace SiteSondage.Controllers
 {
     public class SondageController : Controller
     {
-        // GET: Sondage
+        const string ChaineConnexionBDD = "Data Source=ADMIN-PC;Initial Catalog = SondageBDD; Integrated Security = True";
         public ActionResult PageAccueil()
         {
             return View();
         }
         public ActionResult PageSondage()
         {
-            return View();
+            return View(RecupererEnBdd());
         }
         public ActionResult PageChoix()
         {
@@ -27,7 +29,7 @@ namespace SiteSondage.Controllers
         }
         public ActionResult PageVoter()
         {
-            return View();
+            return View(RecupererEnBdd());
         }
         public ActionResult PageVoteEffectue()
         {
@@ -35,7 +37,43 @@ namespace SiteSondage.Controllers
         }
         public ActionResult PageResultat()
         {
-            return View();
+            return View( RecupererEnBdd());
         }
+        public ClassSondage RecupererEnBdd()
+        {
+            using (SqlConnection connection = new SqlConnection(ChaineConnexionBDD))
+            {
+                connection.Open();
+
+                SqlCommand requeteSQL = new SqlCommand(@"SELECT Question,Choix1,Choix2,Choix3,Choix4,ChoixMultiple,NombreDeVotant,ResultatChoix1,ResultatChoix2,ResultatChoix3,ResultatChoix4 FROM Sondage", connection);
+                
+             
+                SqlDataReader reader = requeteSQL.ExecuteReader();
+
+                reader.Read();
+
+
+                string question = reader.GetString(0);
+                string choix1 = reader.GetString(1);
+                string choix2 = reader.GetString(2);
+                string choix3 = reader.GetString(3);
+                string choix4 = reader.GetString(4);
+                bool choixMultiple = reader.GetBoolean(5);
+                int nombreVotant = reader.GetInt32(6);
+                int resultatChoix1 = reader.GetInt32(7);
+                int resultatChoix2 = reader.GetInt32(8);
+                int resultatChoix3 = reader.GetInt32(9);
+                int resultatChoix4 = reader.GetInt32(10);
+
+
+
+                ClassSondage sondage = new ClassSondage(question, choix1, choix2, choix3, choix4, choixMultiple, nombreVotant,resultatChoix1,resultatChoix2,resultatChoix3,resultatChoix4);
+
+                return sondage;
+
+            }
+
+        }
+        
     }
 }
--- SiteSondage/Models/ClassSondage.cs
+++ SiteSondage/Models/ClassSondage.cs
@@ -12,20 +12,28 @@ namespace SiteSondage.Models
         public string Choix2 { get; private set; }
         public string Choix3 { get; private set; }
         public string Choix4 { get; private set; }
-
-        public int IdSondage { get; private set; }
-
         public bool ChoixMultiple { get; private set; }
 
-        public ClassSondage(string question, string choix1, string choix2, string choix3, string choix4, int idSondage, bool choixMultiple)
+        public int NombreVotant { get; private set; }
+
+        public int ResultatChoix1 { get; private set; }
+        public int ResultatChoix2 { get; private set; }
+        public int ResultatChoix3 { get; private set; }
+        public int ResultatChoix4 { get; private set; }
+
+        public ClassSondage(string question, string choix1, string choix2, string choix3, string choix4, bool choixMultiple, int nombreVotant, int resultatChoix1, int resultatChoix2, int resultatChoix3, int resultatChoix4)
         {
             Question = question;
             Choix1 = choix1;
             Choix2 = choix2;
             Choix3 = choix3;
             Choix4 = choix4;
-            IdSondage = idSondage;
             ChoixMultiple = choixMultiple;
+            NombreVotant = nombreVotant;
+            ResultatChoix1 = resultatChoix1;
+            ResultatChoix2 = resultatChoix2;
+            ResultatChoix3 = resultatChoix3;
+            ResultatChoix4 = resultatChoix4;
         }
     }
 }
